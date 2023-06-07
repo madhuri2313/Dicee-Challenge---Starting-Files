9fdf293e23a415b81b1b1d41818a721b71a015f2
madhuri2313 <madhurisharma.se@gmail.com>
2023-06-07 06:02:23 +0530

    2nd-commit

--- dicee.js
+++ dicee.js
@@ -1,5 +1,25 @@
-var randomNumber1 = Math.floor((Math.random() * 6)+ 1);
+var myRandom1 = Math.floor(Math.random() * 6) + 1;
 
-var imageSource1 = "images/dice" + randomNumber1 +".png";
+diceImage1 = "images/dice" + myRandom1 + ".png"
 
-document.querySelectorAll("image")[0].setAttribute("src", imageSource1);
+document.querySelectorAll("img")[0].setAttribute("src", diceImage1)
+
+var myRandom2 = Math.floor(Math.random() * 6) + 1
+
+diceImage2 = "images/dice" + myRandom2 + ".png"
+
+document.querySelectorAll("img")[1].setAttribute("src", diceImage2)
+
+
+if (myRandom1 > myRandom2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+
+else if (myRandom2 > myRandom1) {
+    document.querySelector("h1").innerHTML = "player 2 Wins";
+}
+
+else {
+    document.querySelector("h1").innerHTML = "DRAW";
+}
+
